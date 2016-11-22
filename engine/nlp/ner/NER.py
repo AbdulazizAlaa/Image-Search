e136a70f7e0d5d1059469e6fa65007aece512b2e
Omar Essam <omar_2412@live.com>
2016-11-22 16:09:48 +0200

    First version

--- engine/nlp/ner/NER.py
+++ engine/nlp/ner/NER.py
@@ -1,3 +1,28 @@
+from pycorenlp import StanfordCoreNLP
 
-def solve(text):
-    return "PERSON!"
+def solve(text, url='http://localhost', port='9000'):
+    text = preprocess(text)
+    nlp = StanfordCoreNLP(url + ":" + port)
+    output = ""
+    while type(output) is not dict:
+        output = nlp.annotate(text, properties={"annotators": "ner", "outputFormat": "json"})
+    persons = []
+    for sentence in output["sentences"]:
+        for token in sentence["tokens"]:
+            if (token["ner"] == "PERSON"):
+                persons.append(token["originalText"])
+    return persons
+
+def preprocess(text):
+    text = text.lower().split(" ")
+    # And rule
+    text = findAndTitle(text, "and")
+    text = findAndTitle(text, "with")
+    return " ".join(text)
+
+def findAndTitle(array, keyword):
+    for idx, item in enumerate(array):
+        if item == keyword:
+            array[idx + 1] = array[idx + 1].title()
+    return array
+
