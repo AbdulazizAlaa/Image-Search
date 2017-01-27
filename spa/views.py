0b291355a7b583db687c343f9b17625ef1d9af33
Omar Essam <omar_2412@live.com>
2017-01-27 18:27:32 +0200

    Fixed unimported templateview

--- spa/views.py
+++ spa/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import permissions
 from rest_framework.views import APIView
+from django.views.generic import TemplateView
 
 #single page app view
 class homePage(APIView):
