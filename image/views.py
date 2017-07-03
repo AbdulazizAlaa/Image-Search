fc00f022c1237c5632710838f10387d3a2b4e407
hadeerKarem <hadeer.karem95@gmail.com>
2017-07-03 17:19:57 +0200

    predicting tag text

--- image/views.py
+++ image/views.py
@@ -216,3 +216,8 @@ class getUsername(APIView):
         text = {'results': list(search)}
         print text
         return Response(text)
+
+class getTextTag(APIView):
+	def get(self, request):
+		q = request.GET.get("q")
+		search = Tag.objects.filter(tag__icontains=q).values_list('tag', flat=True).distinct()
